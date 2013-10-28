bdaab8ee51a862f7b6178f6da282527c722aa67b
sunyifan112358 <sunyifan112358@gmail.com>
2013-10-28 16:45:21 -0400

    removed build

--- build/include/gnuradio/ieee802_11/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yifansun/gr-ieee802-11/include/gnuradio/ieee802_11
-# Build directory: /home/yifansun/gr-ieee802-11/build/include/gnuradio/ieee802_11
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
